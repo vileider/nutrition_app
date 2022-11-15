c1b32cce0e90525f44e3647f1146cf2c48e6cda2
Lauren Keenan <19015831@uhi.ac.uk>
2022-11-15 13:16:01 +0000

    Replaces react-native-windows app
    Initialized correctly, should now run. See readme

---
+++ tay_desktop/windows/tay_desktop/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::tay_desktop::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::tay_desktop::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
