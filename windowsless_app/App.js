64671ed5ccd20590e839e18f9c0c5bfcfa3eceab
Lauren Keenan <19015831@uhi.ac.uk>
2022-11-28 13:15:37 +0000

    returns routing to app.js

--- windowsless_app/App.js
+++ windowsless_app/App.js
@@ -6,32 +6,30 @@
  * @flow strict-local
  */
 
-import React from 'react';
+ import React from 'react';
 
-/* from navigation reactnative docs */
-
-import { NavigationContainer } from '@react-navigation/native';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import 'react-native-gesture-handler';
-
-// import HomePage from './pages/Home';
-// import AboutPage from './pages/About';
-import Navbar from './components/Navbar';
-
-// const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      {/* <Stack.Navigator> */}
-        <Navbar/>
-        {/* <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="About" component={AboutPage} /> */}
-      {/* </Stack.Navigator> */}
-    </NavigationContainer>
-  );
-}
-
-export default App;
+ /* from navigation reactnative docs */
+ 
+ import { NavigationContainer } from '@react-navigation/native';
+ 
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+ import 'react-native-gesture-handler';
+ 
+ import HomePage from './pages/Home';
+ import AboutPage from './pages/About';
+ 
+ const Stack = createNativeStackNavigator();
+ 
+ function App() {
+   return (
+     <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name="HomePage" component={HomePage} />
+         <Stack.Screen name="AboutPage" component={AboutPage} />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ }
+ 
+ export default App;
