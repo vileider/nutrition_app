cb782af8fb8f5169d908e2c13b8c36871eb10d23
vileider <andrewbagio@yahoo.com>
2022-12-01 10:26:52 +0000

    product and calories functionality

--- windowsless_app/components/ProductsDownload.js
+++ windowsless_app/components/ProductsDownload.js
@@ -1,9 +1,24 @@
+import { ApolloClient,
+     InMemoryCache,
+     ApolloProvider,
+     HttpLink,
+     from,
+    } from '@apollo/client';
+    import {ErrorLink, onError} from '@apollo/client/link/error';
+
+const link = from(
+    ErrorLink,
+    new HttpLink({url})
+)
 
 
-
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link
+})
 
 export default function ProductsDownload(){
 
-    
+
 
 }
--- windowsless_app/pages/Food.js
+++ windowsless_app/pages/Food.js
@@ -1,11 +1,44 @@
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
+import {useEffect, useState} from 'react';
+import React from 'react';
 
 const FoodPage = () => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Food Screen</Text>
-      </View> 
-    );
-  };
+  const [foods, setFoods] = useState("123")
+  const [name, setName] = useState("tomato")
+  const apiUrl = `https://api.edamam.com/api/nutrition-data?app_id=0fa1cc30&app_key=fa500a373a17b3793f30066f46c71a92%09&nutrition-type=logging&ingr=${name}`
+useEffect( () => { 
+  async function fetchData() {
+      try {
+          const res = await fetch(apiUrl,
+                 {
+                    method: 'GET',
+                    headers: {
+                       'Content-Type': 'application/json',
+                    },
+                 })
+          const data = await res.json()
+          setFoods(data.calories)
+      } catch (err) {
+          console.log(err);
+      }
+  }
+  fetchData();
+}, [name])
+console.log(foods)
+  return (
+    <View>
+      <TextInput
+          
+          onChangeText={ (text) => setName(text)}          
+          value={name}
+          
+        />
+        
+        <Text >type of food: {name}</Text>
+        <Text >Amount of calories{foods}</Text>
+        </View>
+    
+  )
+      };
 
   export default FoodPage;
